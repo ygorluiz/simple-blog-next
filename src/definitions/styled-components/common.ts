3fd89c2512671497f9a027ede50a5f9fc4d79e9d
ygor <yluiz684@gmail.com>
2021-02-17 20:25:03 -0300

    initial commit with superplate

---
+++ src/definitions/styled-components/common.ts
@@ -0,0 +1,22 @@
+// #region Global Imports
+import { BaseTheme } from "styled-components";
+// #endregion Global Imports
+
+const common: BaseTheme = {
+    colors: {
+        transparent: "transparent",
+        darkGrey: "#282C34",
+        blackGrey: "#20232A",
+        white: "#FFFFFF",
+        black: "rgba(0, 0, 0, 1)",
+        purple: "rgba(180, 59, 255, 1)",
+        rose: "rgba(255, 69, 126, 1)",
+        orange: "rgba(255, 83, 83, 1)",
+        mango: "rgba(255, 143, 74, 1)",
+        mustard: "rgba(255, 219, 69, 1)",
+        blue: "rgba(70, 148, 255, 1)",
+        green: "rgba(48, 229, 132, 1)",
+    },
+};
+
+export { common };
